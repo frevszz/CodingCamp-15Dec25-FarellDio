5ab1ca0b4af9500034265451a32cf66ecea7146f
Farell Dio Revianzha <farelldio1@gmail.com>
2025-12-19 14:50:16 +0700

    web update

--- js/script.js
+++ js/script.js
@@ -8,16 +8,30 @@ function greet() {
   document.getElementById("username").innerText = username;
 }
 
-/// Validate Form
-function validateForm() {
-  let name = document.getElementById("name-input").value;
+// Submit Form
+function submitForm() {
+  const name = document.getElementById("name-input").value;
+  const date = document.getElementById("date-input").value;
+  const message = document.getElementById("message-input").value;
 
-  ///Validate name input
-  if (name === "") {
-    ///alert name empty
-    alert("Name must be filled out");
-  } else {
-    ///alert name valid
-    alert("Thank you, " + name + "");
+  const genderInput = document.querySelector('input[name="gender"]:checked');
+
+  if (!name || !date || !message || !genderInput) {
+    alert("Tolong Isi Semua Kolom!");
+    return;
   }
+
+  const gender = genderInput.value;
+
+  const now = new Date();
+  const formattedTime = now.toString();
+
+  // tampilkan ke output
+  document.getElementById("current-time").innerText =
+    "Current time : " + formattedTime;
+
+  document.getElementById("out-name").innerText = name;
+  document.getElementById("out-date").innerText = date;
+  document.getElementById("out-gender").innerText = gender;
+  document.getElementById("out-message").innerText = message;
 }
